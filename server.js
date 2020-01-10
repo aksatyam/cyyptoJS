41235f954ee6b0d2009b1ec397cddd8e2bb2a84d
aksatyam <ashish.satyam@fancymonk.com>
2020-01-10 16:10:37 +0530

    services updated

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const AES = require("crypto-js/aes");
 const CryptoJS = require("crypto-js");
 
-const ciphertext = "RclT06W1HNSVxqj9Qtjm6qkgQf2h3nSKYZwcmi4hbbbTTotrvaP6QJQ8xQ0wEWe/"
+const ciphertext1 = "a0w9Ed6tgSftWhH+JWLcDw=="
+const ciphertext2 = "mlic@1234"
+
 const key = CryptoJS.enc.Utf8.parse("8080808080808080");
 const iv = CryptoJS.enc.Utf8.parse("9090909090909090");
 
-const decreptedData = AES.decrypt(ciphertext, key, {
+const decreptedData = AES.decrypt(ciphertext1, key, {
+    keySize: 128 / 8,
+    iv: iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7
+});
+const data = decreptedData.toString(CryptoJS.enc.Utf8);
+console.log(`decreptedData ${data}`);
+
+const encryptedData = AES.encrypt(ciphertext2, key, {
     keySize: 128 / 8,
     iv: iv,
     mode: CryptoJS.mode.CBC,
     padding: CryptoJS.pad.Pkcs7
 });
 
-const data = decreptedData.toString(CryptoJS.enc.Utf8);
-console.log(data);
+console.log(`encryptedData ${encryptedData}`);
